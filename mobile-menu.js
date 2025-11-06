79b55fbee69cf71db5af737af05e7ff9b2057fd3
elyder <lyder@kleppnett.no>
2025-11-06 16:53:34 +0100

    Updates

---
+++ mobile-menu.js
@@ -0,0 +1,8 @@
+const menuToggle = document.getElementById("menuToggle");
+const mobileMenu = document.getElementById("mobileMenu");
+
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+  document.body.classList.toggle("no-scroll");
+});
