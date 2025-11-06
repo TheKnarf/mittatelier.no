004190b35aa2e62bb64e285c3e10b09b498f13e6
elyder <lyder@kleppnett.no>
2025-11-06 18:08:15 +0100

    Forbedringer til mobilmenyen

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -1,8 +1,18 @@
 const menuToggle = document.getElementById("menuToggle");
 const mobileMenu = document.getElementById("mobileMenu");
+const menuOverlay = document.getElementById("menuOverlay");
 
 menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("active");
   mobileMenu.classList.toggle("active");
+  menuOverlay.classList.toggle("active");
   document.body.classList.toggle("no-scroll");
+});
+
+// Valgfritt: lukk menyen hvis man trykker på overlayet
+menuOverlay.addEventListener("click", () => {
+  menuToggle.classList.remove("active");
+  mobileMenu.classList.remove("active");
+  menuOverlay.classList.remove("active");
+  document.body.classList.remove("no-scroll");
 });
